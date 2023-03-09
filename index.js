bf05a040e4f72e65c3531666b5c117cbca928474
chris bell <elmntwzard@gmail.com>
2023-03-08 20:47:21 -0600

    Adds my file.md

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const value = [];
+const messageNames = [];
+function writeCards(names, value) {
+    for (let i = 0; i < names.length; i++) {
+        messageNames.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);
+        debugger;
+    }
+    return messageNames
+}
+ 
+
+function countDown(number) {
+    let value = number 
+    while (value >= 0) {
+        console.log(value--);
+    }
+    return number;
+}
